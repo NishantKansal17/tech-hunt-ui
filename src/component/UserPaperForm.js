038b6f85ede2097824a5f452168987033fb6de66
Rupali Saini <rusaini@cisco.com>
2019-06-11 20:04:53 +0530

    Modified user exam page

--- src/component/UserPaperForm.js
+++ src/component/UserPaperForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react"
 import axios from "axios"
-// import {Link, hashHistory} from "react-router"
-import {Card, Form} from "react-bootstrap"
+import {hashHistory} from "react-router"
+import {Card, Button, Form} from "react-bootstrap"
 //import Question from "./question"
 
 import '../css/app.css'
@@ -11,10 +11,12 @@ class UserPaperForm extends Component {
   constructor() {
     super()
     this.state = {
-       questions: []
+       questions: [],
+       answers: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onInput = this.onInput.bind(this);
     this.buildRadioButtons = this.buildRadioButtons.bind(this);
   }
   componentDidMount() {
@@ -27,8 +29,10 @@ class UserPaperForm extends Component {
      const questions = response.data.questions;
      console.log("const questions is: ", questions);
      const updatedQuestions = questions.map(question => {
-       return {
-         ...question
+       if (question != null) {
+         return {
+           ...question
+         }
        }
      });
      this.setState({questions: updatedQuestions});
@@ -36,9 +40,32 @@ class UserPaperForm extends Component {
     })
   }
 
-  handleSubmit (event) {
-
+  handleSubmit(event) {
+    event.preventDefault()
+    let data = {
+      submitterId: localStorage.getItem("userId"),
+      testPaperId: "tim_test",
+      questionEntry: this.state.answers
     }
+    console.log("data submitted is: ", data);
+     const url = `http://localhost:8088/techhunt/testpaper/evaluation/evaluate`;
+     axios.post(
+       url, data, {
+         "crossOrigin": true
+       }
+     ).then(response => {
+       console.log(response)
+       if (response.data.status === "success") {
+         window.alert("Exam submitted successfully!")
+         this.setState({
+           answers: []
+         });
+        //hashHistory.push('/welcome');
+      } else {
+        hashHistory.push('/error');
+      }
+     })
+  }
 
   handleChange (event) {
     const {name, value} = event.target
@@ -50,6 +77,7 @@ class UserPaperForm extends Component {
    return arr.map((choice, i) => {
      return (
        <div key={i}>
+       <Card body style={{width: "80%", marginLeft: "1%", marginBottom:"1%"}}>
          <input
          type={type}
          name={id}
@@ -57,12 +85,49 @@ class UserPaperForm extends Component {
          onChange={this.onInput}
       />
        <label>{choice}</label>
+       </Card>
      </div>
     )
  })
 }
+onInput(e) {
+  console.log("OnInput() > e.target: ", e.target);
+  const id = e.target.name;
+  const type = e.target.type;
+  const answer = { id, questionSolution: "", answersubmitted: e.target.value };
+  let answers;
+  // if (type == "checkbox") {
+  //   if (this.state.answers.filter(answer => answer.id === id)) {
+  //     answers = [...this.state.answers.filter(answer => answer.id === id), answer]
+  //     console.log("filter result> ",this.state.answers.filter(answer => answer.id === id));
+  //     if (this.state.answers.indexOf(answer.id) >= 0) {
+  //       //answers = [...this.state.answers.indexOf(answer.id === id)].concat(answer);
+  //       console.log("after concat ansz is: ", answer);
+  //     }
+  //     console.log("chkbox answer is: ", answer);
+  //   }
+  // } else {
+      let index = this.state.questions.findIndex(el => el.questionId === id);
+      console.log("index found is: ", index);
+      answer.questionSolution = this.state.questions[index].questionSolution;
+
+      if (this.state.answers.some(answer => answer.id === id)) {
+        console.log("loop1");
+        answers = [...this.state.answers.filter(answer => answer.id !== id), answer];
+        console.log("...this.state.answers is: ", [...this.state.answers]);
+      } else {
+        console.log("loop2");
+        answers = [...this.state.answers, answer];
+      }
+
+      //answers[index] = {...answers[index], questionSolution: this.state.questions[index].questionSolution};
+      //this.setState({ markers });
+    //}
+    this.setState({ answers }, () => console.log("answers are: ", this.state.answers));
+  }
   render() {
     var iterator = this.state.questions.map((question, i) => {
+      if (question != null) {
           var type = "";
           if (question.multiQuestion) {
             type = "checkbox"
@@ -71,20 +136,23 @@ class UserPaperForm extends Component {
           }
           return (
             <div key={question.questionId}>
-              <h4>{question.questionDescription}</h4>
-              {this.buildRadioButtons(question.questionOptions, type, question.questionId)}
+              <h4>Question: {question.questionDescription}</h4>
+              <h4>Options: </h4>{this.buildRadioButtons(question.questionOptions, type, question.questionId)}
             </div>
           );
+        }
         });
         return (
-          <Card style={{width: "100rem", "margin-left": "12%"}}>
+          <Card style={{width: "100rem", marginLeft: "12%"}}>
             <Card.Header as="h5">Tech Know Exam</Card.Header>
             <Card.Body>
-              <Card.Title>Questions</Card.Title>
                 <Form style={{width: "100%"}}>
                     <div className="h4">
                       {iterator}
                     </div>
+                    <Button variant="primary" type="button" size="lg" onClick={this.handleSubmit}>
+                      Submit
+                    </Button>
                 </Form>
             </Card.Body>
           </Card>
